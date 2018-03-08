437dea1f9e9b836fcb6a3e1a2ae9693b688f7046
chenxin008 <chenxin008@vip.qq.com>
2018-03-08 15:11:52 +0800

    初始化本地仓库

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,32 @@
+﻿using Hangfire;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using LuaTableToCsSharp;
+
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string luatable = @"{1=0,2=0,3=0,4=0,5={},6=0,7={s010GameConfig={s008wPayType=0,s009wCostType=2,s015dwReservedRule2=2,s006ClubId=0,s010wCostValue=0,s010wCellScore=1,s014wPlayCountRule=8,s013wHadPlayCount=0,s010dwPlayRule=738459648,s010wSubGameID=112,s009wMaxScore=0,s015dwReservedRule1=0,s015sPrivateTableID=0},s006GameID=112},8=1}";
+
+            SharpluaTable lua = new SharpluaTable();
+            var dic = lua.Parse(luatable.Trim());
+            Console.WriteLine(dic["7"]);
+            SharpluaTable luaitem = new SharpluaTable();
+            var items = luaitem.Parse(dic["7"]);
+            var gameconfig = new SharpluaTable().Parse(items["s010GameConfig"]);
+
+            string str = "{s010GameConfig={s008wPayType=0,s009wCostType=2,s015dwReservedRule2=2,s006ClubId=0,s010wCostValue=0,s010wCellScore=1,s014wPlayCountRule=8,s013wHadPlayCount=0,s010dwPlayRule=738459648,s010wSubGameID=112,s009wMaxScore=0,s015dwReservedRule1=0,s015sPrivateTableID=0},s006GameID=112}";
+            var item = new SharpluaTable().Parse(str);
+
+        }
+    }
+
+    
+}
---
+++ LuaTableToCSharp/SharpluaTable.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LuaTableToCsSharp
+{
+    /// <summary>
+    /// lua table转C# Dictionary
+    /// </summary>
+    public class SharpluaTable
+    {
+        string luatable = "";
+
+        //从{开始解析
+
+        //然后按等号分割，等号前后为一个键值对
+
+        //逗号之后，为另一个键值对
+
+        //如果遇到中途遇到{，一直解析到}为止,都为值
+
+
+        Dictionary<string, string> dic = new Dictionary<string, string>();
+
+        /// <summary>
+        /// luatable 反序列化后的字符串
+        /// </summary>
+        /// <param name="luatable"></param>
+        /// <returns></returns>
+        public Dictionary<string, string> Parse(string luatable)
+        {
+            this.luatable = luatable;
+            //解析0位和最后一位，判断是否是luatable格式
+
+            if (luatable[0] != '{')
+            {
+                throw new Exception("Parse failure， format error(Must begin with {)");
+            }
+
+            if (luatable[luatable.Length - 1] != '}')
+            {
+                throw new Exception("Parse failure， format error(Must end with})");
+            }
+            string luaKey = string.Empty, LuaValue = string.Empty;
+            //标示解析Key还是Value，如果true，那么解析到Key里面，如果是false，那么解析到value里面
+            bool iskey = true;
+
+
+            for (int i = 1; i < luatable.Length; i++)
+            {
+                //如果是最后一个键值对，那么直接就完了
+                if (i+1==luatable.Length&&luatable[i]=='}')
+                {
+                    dic.Add(luaKey, LuaValue);
+                    luaKey = string.Empty;
+                    LuaValue = string.Empty;
+                    
+                    break;
+                }
+                //如果是逗号，那么存储当前的key value ,跳过当前字符解析
+                if (luatable[i] == ',')
+                {
+                    dic.Add(luaKey, LuaValue);
+                    luaKey = string.Empty;
+                    LuaValue = string.Empty;
+                    iskey = true;  //跳过一个逗号，那么继续解析为key
+                    continue;
+                }
+                else
+                {
+                    if (luatable[i] == '=')
+                    {
+                        iskey = false; //如果是等号，那么解析为key,并跳过当前
+                        continue;
+                    }
+                    //如果是二级的{，那么解析到}为止，并把当前的i的值移动到}的下标位置
+                    if (luatable[i] == '{')
+                    {
+                        //LuaValue += luatable[i];
+                        int kuohaoCount = 0;
+                        for (int j = i; j < luatable.Length; j++)
+                        {
+                            LuaValue += luatable[j];
+                            if (luatable[j] == '{')
+                            {
+                                kuohaoCount += 1;
+                            }
+                            if (luatable[j] == '}')
+                            {
+                                kuohaoCount -= 1;
+
+                                if (kuohaoCount == 0)
+                                {
+                                    i = j;
+                                    break;
+                                }
+                                else
+                                {
+                                    //kuohaoCount -= 1;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (iskey)
+                        {
+                            luaKey += luatable[i];
+                        }
+                        else
+                        {
+                            LuaValue += luatable[i];
+                        }
+                    }
+
+                }
+            }
+
+            return dic;
+
+        }
+
+
+
+
+    }
+}
